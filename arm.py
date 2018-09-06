ec2b0b1cd0735c65ab770d8091510775fb262e49
supriyamarturi <42571888+supriyamarturi@users.noreply.github.com>
2018-09-05 22:51:33 -0700

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,8 +1,10 @@
 lower,upper=map(int,raw_input().split())
-for n in range(lower+1,upper):
-    if(n>1):
-        for i in range(2,n):
-            if(n % i)==0:
+for m in range(lower+1,upper):
+    if(m>1):
+        for i in range(2,m):
+            if(m % i)==0:
                 break
         else:
-            print(n)
+            print(m)
+
+
