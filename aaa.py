ef023fe4188ba0cfdf6c1060861166483386777b
supriyamarturi <42571888+supriyamarturi@users.noreply.github.com>
2018-09-06 00:07:32 -0700

    Create aaa.py

---
+++ aaa.py
@@ -0,0 +1,18 @@
+lower,upper=map(int,raw_input().split())
+for num in range(lower, upper + 1):
+
+   
+   order = len(str(num))
+    
+   
+   sum = 0
+
+   
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)
