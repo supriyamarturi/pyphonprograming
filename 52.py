dec9c60762aa3698791666eedba1932f82cda2fd
supriyamarturi <42571888+supriyamarturi@users.noreply.github.com>
2018-09-19 11:18:57 +0530

    Update 52.py

--- 52.py
+++ 52.py
@@ -5,12 +5,7 @@ def switch_demo(argument):
         3: "Three",
         4: "Four",
         5: "Five",
-        6: "Six",
-        7: "Seven",
-        8: "Eight",
-        9: "Nine",
-        10: "Ten"
     }
     print switcher.get(argument, "Invalid Input")
-n=int(raw_input())
-switch_demo(n)
+m=int(raw_input())
+switch_demo(m)
