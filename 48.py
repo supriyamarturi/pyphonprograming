90df61f7034420b99a451fff2703dcbaf54bd622
supriyamarturi <42571888+supriyamarturi@users.noreply.github.com>
2018-09-19 10:48:45 +0530

    Update 48.py

--- 48.py
+++ 48.py
@@ -1,4 +1,4 @@
-n=int(raw_input())
-list=[int(a) for a in raw_input().split()]
-average=sum(list)/n
+a=int(raw_input())
+list=[int(s) for s in raw_input().split()]
+average=sum(list)/a
 print average
