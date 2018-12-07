3efa405c472a22803df658bb24aba696fe9bfb85
supriyamarturi <42571888+supriyamarturi@users.noreply.github.com>
2018-12-07 11:11:18 +0530

    Create pl8.py

---
+++ pl8.py
@@ -0,0 +1,12 @@
+input = raw_input("Enter a string \n").lower()  
+words = input.split(' ')
+capitalized = []  
+for word in words:
+    final_words = word[0].upper() + word[1:] 
+    capitalized.append(final_words) 
+output = ' '.join(capitalized) 
+print output 
+
+
+
+
