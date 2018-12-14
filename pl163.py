b86672d057a9ef1e4c51f8c38c61dee465d06662
supriyamarturi <42571888+supriyamarturi@users.noreply.github.com>
2018-12-14 20:18:33 +0530

    Create pl163.py

---
+++ pl163.py
@@ -0,0 +1,28 @@
+import sys,string, math,itertools
+
+s,a = raw_input().split()
+s,a = int(s),int(s)
+L = [ int(x) for x in raw_input().split()]
+if a in L :
+    print('yes')
+else :
+    print('no')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
