8346f5c14a55c3e568ed1911066c3da00ecc988c
supriyamarturi <42571888+supriyamarturi@users.noreply.github.com>
2018-12-15 10:35:01 +0530

    Update and rename pl54.py to player54.py

--- pl54.py
+++
@@ -1,5 +0,0 @@
-s1,s2=map(str,raw_input().split())
-if(s1==s2):
-    print "yes"
-else:
-    print "no"
---
+++ player54.py
@@ -0,0 +1,6 @@
+m1,m2=map(str,raw_input().split())
+if(m1==m2):
+    print "yes"
+else:
+    print "no"
+
