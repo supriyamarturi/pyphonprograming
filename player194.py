a502c8034a1dc82dad23767fe273262314abd741
supriyamarturi <42571888+supriyamarturi@users.noreply.github.com>
2018-12-17 11:56:48 +0530

    Update player194.py

--- player194.py
+++ player194.py
@@ -1,13 +1,9 @@
-s,d=map(str,raw_input().split())
-if s=='P' and d=='R' or s=='R' and d=='P':
-    print('P')
-elif s=='P' and d=='S' or s=='S' and d=='P':
-    print('S')
-elif s=='S' and d=='R' or s=='R' and d=='S':
+c1,d1 = raw_input().split()
+if c1==d1:
+    print('D')
+elif c1=='R' and d1=='S':
     print('R')
-elif s=='S' and d =='S':
-    print('D')
-elif s=='P' and d=='P':
-    print('D')
-elif s=='R' and d=='R':
-    print('D')
+elif c1=='R' and d1=='P':
+    print('P')
+else:
+    print('S')
